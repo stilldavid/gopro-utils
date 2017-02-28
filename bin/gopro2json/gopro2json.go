cb4886683b0e972545fdc00a930155b077c866c2
Brian Foshee <brianfoshee@gmail.com>
2017-02-27 21:22:48 -0500

    gopro2json: use encoding/json to encode telemetry data

--- bin/gopro2json/gopro2json.go
+++ bin/gopro2json/gopro2json.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/stilldavid/gopro-utils/telemetry"
 )
+
+type data struct {
+	Data []telemetry.TELEM_OUT `json:"data"`
+}
 
 func main() {
 	inName := flag.String("i", "", "Required: telemetry file to read")
@@ -23,42 +29,19 @@ func main() {
 		fmt.Printf("Cannot access telemetry file %s.\n", *inName)
 		os.Exit(1)
 	}
+	defer telemFile.Close()
 
-	jsonFile, err := os.Create(*outName)
-	if err != nil {
-		fmt.Printf("Cannot make output file %s.\n", *outName)
-		os.Exit(1)
-	}
-
-	defer func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-			fmt.Printf("Cannot close file %s: %s", file.Name(), err)
-			os.Exit(1)
-		}
-	}(telemFile)
-
-	defer func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-			fmt.Printf("Cannot close file %s: %s", file.Name(), err)
-			os.Exit(1)
-		}
-	}(jsonFile)
-
-	jsonFile.WriteString(`{"data":[`)
+	var d data
 
 	t := &telemetry.TELEM{}
 	t_prev := &telemetry.TELEM{}
-	first := true
 
 	for {
 		t, err = telemetry.Read(telemFile)
-		if err != nil {
-			fmt.Println(err)
+		if err != nil && err != io.EOF {
+			fmt.Println("Error reading telemetry file", err)
 			os.Exit(1)
-		}
-		if t == nil {
+		} else if err == io.EOF || t == nil {
 			break
 		}
 
@@ -72,16 +55,29 @@ func main() {
 		// process until t.Time
 		t_prev.Process(t.Time.Time)
 
-		buf, _ := t_prev.ShitJson(first)
-		jsonFile.WriteString(buf.String())
-		if first {
-			first = false
-		}
+		telems := t_prev.ShitJson()
+		d.Data = append(d.Data, telems...)
 
 		*t_prev = *t
 		t = &telemetry.TELEM{}
 	}
 
-	// end the bogusness
-	jsonFile.WriteString(`]}`)
+	jsonFile, err := os.Create(*outName)
+	if err != nil {
+		fmt.Printf("Cannot make output file %s.\n", *outName)
+		os.Exit(1)
+	}
+
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			fmt.Printf("Cannot close json file %s: %s", file.Name(), err)
+			os.Exit(1)
+		}
+	}(jsonFile)
+
+	if err := json.NewEncoder(jsonFile).Encode(d); err != nil {
+		fmt.Println("Error encoding output json", err)
+		os.Exit(1)
+	}
 }
